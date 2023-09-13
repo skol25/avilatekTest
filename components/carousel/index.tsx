3cdf64c1ab1da958d13e3bb8fc99bcbb0abc323d
skol25 <andres.lobolugo25@gmail.com>
2023-09-13 16:10:02 -0400

    carousel fix

--- components/carousel/index.tsx
+++ components/carousel/index.tsx
@@ -3,27 +3,29 @@ import React, { useState } from 'react';
 const Carousel = ({ slides }) => {
   // Estado para rastrear el índice actual
   const [currentIndex, setCurrentIndex] = useState(0);
+  const [currentImage, setCurrentImage] = useState('');
 
   // Función para cambiar al índice seleccionado
   const goToSlide = (index) => {
     setCurrentIndex(index);
+    setCurrentImage(slides[index].image);
   };
 
   return (
     <div className="relative">
       <div className="relative overflow-hidden">
         <div className="flex space-x-4 transition-transform ease-in-out duration-300">
-          {slides.map((slide, index) => (
+          {slides.map((slide, index: number) => (
             <div
               key={index}
               className={`w-full h-[450px] flex-shrink-0 relative ${
                 currentIndex === index
                   ? 'transform translate-x-0' // Si es el índice actual, muestra la imagen
-                  : 'transform translate-x-full' // Si no es el índice actual, oculta la imagen
+                  : 'transform translate-x-1' // Si no es el índice actual, oculta la imagen
               }`}
             >
               <img
-                src={slide.image}
+                src={currentImage === '' ? slide.image : currentImage}
                 alt={slide.caption}
                 className="w-full h-full object-cover"
               />
@@ -37,7 +39,7 @@ const Carousel = ({ slides }) => {
           <div
             key={index}
             onClick={() => goToSlide(index)} // Cambia al índice seleccionado al hacer clic
-            className={`w-16 h-2 mr-3  rounded-full cursor-pointer transition-transform ${
+            className={`w-16 h-2   rounded-full cursor-pointer transition-transform ${
               currentIndex == index ? 'scale-125 bg-[#E2081E]' : 'bg-gray-300' // Resalta el indicador si es el índice actual
             }`}
           />
